8ccc6d804367a94aa8f4b4a2029e3c57f712f099
threebrick <darnell@threebrick.com>
2017-05-23 23:58:32 -0400

    Added more content

--- app.js
+++ app.js
@@ -18,10 +18,13 @@ var connector = new builder.ChatConnector({
 
 // Listen for messages from users 
 //server.post('/api/messages', connector.listen());
-server.post('https://agile-headland-89882.herokuapp.com/api/messages', connector.listen());
+server.post('https://hollerskill.azurewebsites.net/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector);
+
+bot.beginDialogAction('restaurants', '/restaurants', { matches: /^restaurants|find restaurants|search restaurants/i });
+bot.beginDialogAction('events', '/events', { matches: /^events|find events|search events/i });
 
 bot.dialog('/', [
     function (session) {
@@ -30,8 +33,8 @@ bot.dialog('/', [
       //      inputHint: builder.InputHint.expectingInput
       //  });
         builder.Prompts.text(session, 'Hi there.  What is your name?', {            
-        speak: 'Hey there mister.  What is <emphasis level=\"moderate\">your name dude</emphasis>?',
-        retrySpeak: 'I did not understand my man.  Say <emphasis level=\"moderate\">your name fool</emphasis>',
+        speak: 'Hey there mister.  What is your name ?',
+        retrySpeak: 'I did not understand my man.  Say your name?',
         inputHint: builder.InputHint.expectingInput
         });
     },
@@ -40,11 +43,54 @@ bot.dialog('/', [
         console.log(session.message.text);
 
         builder.Prompts.text(session, 'You said '+ session.message.text, {            
-        speak: 'You said <emphasis level=\"moderate\">' + session.message.text +'</emphasis>?'
+        speak: 'You said ' + session.message.text +''
         
         });
     }
 ]);
+
+bot.dialog('/restaurants', [
+    function (session) {
+
+    //   session.userData.faqTest = "startFAQ"; 
+
+       var msg = new builder.Message(session)
+            .attachments([
+                new builder.HeroCard(session)
+                    .title("Hero Card")
+                    .subtitle("The Space Needle is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
+                    .images([
+                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
+                    ])
+                    .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle"))
+            ]);
+        //session.send(msg);
+        session.endDialog(msg);
+    }
+]);
+bot.beginDialogAction('restaurants', '/restaurants'); 
+
+
+bot.dialog('/events', [
+    function (session) {
+
+    //   session.userData.faqTest = "startFAQ"; 
+
+        msg = new builder.Message(session)
+            .attachments([
+                new builder.ThumbnailCard(session)
+                    .title("Thumbnail Card")
+                    .subtitle("Pike Place Market is a public market overlooking the Elliott Bay waterfront in Seattle, Washington, United States.")
+                    .images([
+                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/PikePlaceMarket.jpg/320px-PikePlaceMarket.jpg")
+                    ])
+                    .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Pike_Place_Market"))
+            ]);
+        //session.endDialog(msg);
+        session.endDialog(msg);
+    }
+]);
+bot.beginDialogAction('events', '/events'); 
 
 server.get('/', restify.serveStatic({
  directory: __dirname,
